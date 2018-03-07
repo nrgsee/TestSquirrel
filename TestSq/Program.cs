7231971ce3a44945d003edb4f0be5ce06e16fec5
Daniel Rozputnii <d_rasputnyy@mifprojects.com>
2018-03-07 09:54:33 +0200

    Init sample

---
+++ TestSq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Deployment.Application.ApplicationDeployment;
+
+namespace TestSq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"IsNetworkDeployed: {IsNetworkDeployed}");
+            if (IsNetworkDeployed)
+                Console.WriteLine($"CurrentDeployment: {CurrentDeployment?.CurrentVersion}");
+            Console.WriteLine($"Assembly: {Assembly.GetExecutingAssembly().GetName().Version}");
+            Console.ReadKey();
+        }
+    }
+}
